fc13c286a7857dfc6d113556b11d1f489e51729b
Rachel Shu <rachel@rachelshu.com>
2024-05-13 23:57:27 +0000

    start setting up streamlit server

--- app.py
+++ app.py
@@ -10,3 +10,10 @@ st.set_page_config(
 )
 
 st.write("# Hello! Welcome to Streamlit! 👋")
+
+FILE_DIR = "/workspace/novel-tongue/notebooks/alice_in_wonderland"
+
+c1_en = open(f"{FILE_DIR}/c1_en.txt", "r", encoding="UTF-8").read()
+c1_es = open(f"{FILE_DIR}/c1_es.txt", "r", encoding="UTF-8").read()
+
+st.columns([c1_en, c1_es])
