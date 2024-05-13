f7cc2d8162587aa8604b1824c2053f7087799013
Rachel Shu <rachel@rachelshu.com>
2024-05-12 23:00:38 -0600

    add streamlit demo page

---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+Main application script for the app.
+"""
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Hello! Welcome to Streamlit! 👋")
